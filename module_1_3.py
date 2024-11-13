f336325541634f9e61e3f241c8bf98088b47af9b
Akstorm779 <akstorm779@gmail.com>
2024-11-13 21:52:40 +0300

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,8 @@
+name= 'Igor'
+print(name, type(name))
+age= 30
+print(age, type(age))
+age= age+1
+print(age, type(age))
+is_student= 'true'
+print(is_student, type(is_student))
---
+++ module_1_6.py
@@ -0,0 +1,21 @@
+print('slovary')
+my_dict = {'Igor': 8909123840, 'olga': 8904235798}
+print(my_dict)
+print(my_dict['olga'])
+my_dict['toxa']= 430670987
+print(my_dict)
+my_dict['sofi'] = 4790890874
+del my_dict ['olga']
+my_dict.update({'alex': 83902578967,
+                'krot': 8935902572})
+print(my_dict)
+print(my_dict.keys())
+print('___________')
+print('mnojestva')
+
+my_set = {1,'pro',2,'erh', 1, 'pro', 1, True, (1,2,3)}
+print(my_set)
+print(my_set.add(3))
+print(my_set.add(45))
+print(my_set.discard(2))
+print(my_set)
